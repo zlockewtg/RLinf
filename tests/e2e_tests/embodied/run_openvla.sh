4e04614d18bd82cdef857fb7cb2624e995d78331
Andy Lin <32576375+andylin-hao@users.noreply.github.com>
2025-08-21 00:55:31 +0800

    feat(ci): use docker hub images for testing (#14)
    Signed-off-by: Hao Lin <linhaomails@gmail.com>

--- tests/e2e_tests/embodied/run_openvla.sh
+++ tests/e2e_tests/embodied/run_openvla.sh
@@ -3,19 +3,7 @@ set -x
 
 tabs 4
 
-source /opt/conda/etc/profile.d/conda.sh
-conda activate vla
-apt update && apt-get install -y libglew-dev libegl1 libvulkan1 vulkan-tools
-cp ${REPO_PATH}/tests/e2e_tests/embodied/env_jsons/nvidia_icd.json /usr/share/vulkan/icd.d/nvidia_icd.json
-cp ${REPO_PATH}/tests/e2e_tests/embodied/env_jsons/10_nvidia.json /usr/share/glvnd/egl_vendor.d/10_nvidia.json
-cp ${REPO_PATH}/tests/e2e_tests/embodied/env_jsons/nvidia_layers.json /etc/vulkan/implicit_layer.d/nvidia_layers.json
-python -m mani_skill.examples.demo_random_action
-
-export PYTHONPATH=${REPO_PATH}:${REPO_PATH}/megatron:$PYTHONPATH
+export PYTHONPATH=${REPO_PATH}:$PYTHONPATH
 unset HOME # GitHub action sets HOME to a wrong path (/github/home), breaking simulator
-
-cd /workspace/dataset/repos/openvla-main
-pip install -e .
-cd -
 
 python ${REPO_PATH}/examples/embodiment/train_embodied_agent.py --config-path ${REPO_PATH}/tests/e2e_tests/embodied --config-name ppo_openvla
