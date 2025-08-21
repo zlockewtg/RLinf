b59ef532ba6824a146bdc92cb2a6ecaf0402c28d
Andy Lin <32576375+andylin-hao@users.noreply.github.com>
2025-08-21 10:48:00 +0800

    fix(ci): use Megatron in the docker image (#16)
    Signed-off-by: Hao Lin <linhaomails@gmail.com>

--- tests/e2e_tests/math/run_collocated.sh
+++ tests/e2e_tests/math/run_collocated.sh
@@ -6,6 +6,6 @@ export VLLM_ATTENTION_BACKEND=XFORMERS
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 export TOKENIZERS_PARALLELISM=false
 
-export PYTHONPATH=${REPO_PATH}:${REPO_PATH}/megatron:$PYTHONPATH
+export PYTHONPATH=${REPO_PATH}:$PYTHONPATH
 
 python ${REPO_PATH}/examples/math/main_math.py --config-path $REPO_PATH/tests/e2e_tests/math  --config-name qwen2.5-1.5b-grpo-collocated
