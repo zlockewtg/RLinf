072e54aa54f2aaff4c1e0240e32bd9e555cf397c
Andy Lin <32576375+andylin-hao@users.noreply.github.com>
2025-10-13 01:58:54 +0800

    feat: unfied CI script and new embodied CI tests (#163)
    Signed-off-by: Hao Lin <linhaomails@gmail.com>

--- tests/e2e_tests/embodied/run_openvla.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-set -x
-
-tabs 4
-
-export PYTHONPATH=${REPO_PATH}:$PYTHONPATH
-
-python ${REPO_PATH}/examples/embodiment/train_embodied_agent.py --config-path ${REPO_PATH}/tests/e2e_tests/embodied --config-name ppo_openvla
--- tests/e2e_tests/embodied/run_openvlaoft_libero130.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-set -x
-
-tabs 4
-export MUJOCO_GL="osmesa"
-export PYOPENGL_PLATFORM="osmesa"
-export PYTHONPATH=${REPO_PATH}:$PYTHONPATH
-
-python ${REPO_PATH}/examples/embodiment/train_embodied_agent.py --config-path ${REPO_PATH}/tests/e2e_tests/embodied --config-name libero_130_grpo_openvlaoft
