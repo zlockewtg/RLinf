44822560a5a530fd438ef4e56e695d8d938e9bed
zlock <1840409990@qq.com>
2026-01-09 06:12:43 +0000

    Ignore franka_sim directory
    Signed-off-by: zlock <1840409990@qq.com>

--- rlinf/envs/mujoco/__init__.py
+++ rlinf/envs/mujoco/__init__.py
@@ -1,8 +1,4 @@
-from .serl_franka_lift_cube import SERLFrankaEnv
 from .franka_sim.mujoco_gym_env import GymRenderingSpec, MujocoGymEnv
+from .serl_franka_lift_cube import SERLFrankaEnv
 
-__all__ = [
-
-    "SERLFrankaEnv"
-]
-
+__all__ = ["SERLFrankaEnv", "GymRenderingSpec", "MujocoGymEnv"]
